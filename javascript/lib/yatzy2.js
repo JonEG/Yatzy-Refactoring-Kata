24b22d55f2664078f08621238ca14234091cc4ba
JonEG <echevestegonzalezjon@gmail.com>
2025-05-08 14:20:44 +0200

    Refactored Yatzy, Pair, TwoPairs, ThreeOfAKind, FourOfAKind, FullHouse, SmallStraight, LargeStraight in Yatzy2

--- javascript/lib/yatzy2.js
+++ javascript/lib/yatzy2.js
@@ -1,21 +1,3 @@
-const YatzyCategory = Object.freeze({
-    CHANCE: 'CHANCE',
-    YATZY: 'YATZY',
-    ONES: 'ONES',
-    TWOS: 'TWOS',
-    THREES: 'THREES',
-    FOURS: 'FOURS',
-    FIVES: 'FIVES',
-    SIXES: 'SIXES',
-    PAIR: 'PAIR',
-    THREE_OF_A_KIND: 'THREE_OF_A_KIND',
-    FOUR_OF_A_KIND: 'FOUR_OF_A_KIND',
-    SMALL_STRAIGHT: 'SMALL_STRAIGHT',
-    LARGE_STRAIGHT: 'LARGE_STRAIGHT',
-    TWO_PAIRS: 'TWO_PAIRS',
-    FULL_HOUSE: 'FULL_HOUSE'
-});
-
 class YatzyHand {
     static diceValues = [6, 5, 4, 3, 2, 1];
 
@@ -25,7 +7,7 @@ class YatzyHand {
         }
     }
 
-    static score(dice){
+    static score(dice) {
         // Calculate dice frequencies
         const diceFrequencies = this.calculateDiceFrequencies(dice);
         const total = this.calculate(dice, diceFrequencies);
@@ -54,7 +36,7 @@ class Chance extends YatzyHand {
     }
 }
 
-class Yatzy2 extends YatzyHand {
+class Yatzy extends YatzyHand {
     static calculate(dice, diceFrequencies) {
         // Score for Yatzy if all dice are the same
         let yatzyResult = 0;
@@ -66,177 +48,158 @@ class Yatzy2 extends YatzyHand {
 }
 
 class Ones extends YatzyHand {
-    static calculate(dice, diceFrequencies){
+    static calculate(dice, diceFrequencies) {
         return diceFrequencies[1];
     }
 }
 
 class Twos extends YatzyHand {
-    static calculate(dice, diceFrequencies){
+    static calculate(dice, diceFrequencies) {
         return diceFrequencies[2] * 2;
     }
 }
 
 class Threes extends YatzyHand {
-    static calculate(dice, diceFrequencies){
+    static calculate(dice, diceFrequencies) {
         return diceFrequencies[3] * 3;
     }
 }
 
 class Fours extends YatzyHand {
-    static calculate(dice, diceFrequencies){
+    static calculate(dice, diceFrequencies) {
         return diceFrequencies[4] * 4;
     }
 }
 
 class Fives extends YatzyHand {
-    static calculate(dice, diceFrequencies){
+    static calculate(dice, diceFrequencies) {
         return diceFrequencies[5] * 5;
     }
 }
 
 class Sixes extends YatzyHand {
-    static calculate(dice, diceFrequencies){
+    static calculate(dice, diceFrequencies) {
         return diceFrequencies[6] * 6;
     }
 }
 
-
-
-class Yatzy {
-    #diceValues = [6, 5, 4, 3, 2, 1];
-
-    score(dice, categoryName) {
-        const category = YatzyCategory[categoryName];
-
-        // Calculate dice frequencies
-        const diceFrequencies = this.calculateDiceFrequencies(dice);
-
-        // Calculate the score
-        let result;
-        switch (category) {
-            case YatzyCategory.PAIR:
-                // score pair if two dice are the same
-                let pairResult = 0;
-                // score highest pair if there is more than one
-                for (const i of this.#diceValues) {
-                    if (diceFrequencies[i] >= 2) {
-                        pairResult = i * 2;
-                        break;
-                    }
-                }
-                result = pairResult;
+class Pair extends YatzyHand {
+    static calculate(dice, diceFrequencies) {
+        // score pair if two dice are the same
+        let pairResult = 0;
+        // score highest pair if there is more than one
+        for (const i of this.diceValues) {
+            if (diceFrequencies[i] >= 2) {
+                pairResult = i * 2;
                 break;
-
-            case YatzyCategory.THREE_OF_A_KIND:
-                // score if three dice are the same
-                let threeKindResult = 0;
-                for (const i of this.#diceValues) {
-                    if (diceFrequencies[i] >= 3) {
-                        threeKindResult = i * 3;
-                        break;
-                    }
-                }
-                result = threeKindResult;
-                break;
-            case YatzyCategory.FOUR_OF_A_KIND:
-                // score if four dice are the same
-                let fourKindResult = 0;
-                for (const i of this.#diceValues) {
-                    if (diceFrequencies[i] >= 4) {
-                        fourKindResult = i * 4;
-                        break;
-                    }
-                }
-
-                result = fourKindResult;
-                break;
-
-            case YatzyCategory.SMALL_STRAIGHT:
-                // score if dice contains 1,2,3,4,5
-                let smallStraightResult = 0;
-                let count = 0;
-                for (const frequency of Object.values(diceFrequencies)) {
-                    if (frequency === 1) {
-                        count++;
-                    }
-                }
-                if (count === 5 && diceFrequencies[6] === 0) {
-                    for (const die of dice) {
-                        smallStraightResult += die;
-                    }
-                }
-                result = smallStraightResult;
-                break;
-            case YatzyCategory.LARGE_STRAIGHT:
-                // score if dice contains 2,3,4,5,6
-                let largeStraightResult = 0;
-                let straightCount = 0;
-                for (const frequency of Object.values(diceFrequencies)) {
-                    if (frequency === 1) {
-                        straightCount++;
-                    }
-                }
-                if (straightCount === 5 && diceFrequencies[1] === 0) {
-                    for (const die of dice) {
-                        largeStraightResult += die;
-                    }
-                }
-                result = largeStraightResult;
-                break;
-
-            case YatzyCategory.TWO_PAIRS:
-                // score if there are two pairs
-                let twoPairResult = 0;
-                let pairCount = 0;
-                for (const frequency of Object.values(diceFrequencies)) {
-                    if (frequency >= 2) {
-                        pairCount++;
-                    }
-                }
-
-                if (pairCount === 2) {
-                    for (const i of this.#diceValues) {
-                        if (diceFrequencies[i] >= 2) {
-                            twoPairResult += i * 2;
-                        }
-                    }
-                }
-
-                result = twoPairResult;
-                break;
-
-            case YatzyCategory.FULL_HOUSE:
-                // score if there is a pair as well as three of a kind
-                let fullHouseResult = 0;
-                if (Object.values(diceFrequencies).includes(2) &&
-                    Object.values(diceFrequencies).includes(3)) {
-                    for (const die of dice) {
-                        fullHouseResult += die;
-                    }
-                }
-                result = fullHouseResult;
-                break;
-
-            default:
-                result = 0;
-                break;
+            }
         }
-
-        return result;
-    };
-
-
-    calculateDiceFrequencies(dice) {
-        const diceFrequencies = {};
-        for (const i of this.#diceValues) {
-            diceFrequencies[i] = 0;
-        }
-        for (const die of dice) {
-            diceFrequencies[die] = (diceFrequencies[die] || 0) + 1;
-        }
-        return diceFrequencies;
+        return pairResult;
     }
 }
 
-module.exports = {Yatzy, Chance, Yatzy2, Ones, Twos, Threes, Fours, Fives, Sixes};
+class ThreeOfAKind extends YatzyHand {
+    static calculate(dice, diceFrequencies) {
+        // score if three dice are the same
+        let threeKindResult = 0;
+        for (const i of this.diceValues) {
+            if (diceFrequencies[i] >= 3) {
+                threeKindResult = i * 3;
+                break;
+            }
+        }
+        return threeKindResult;
+    }
+}
+
+class FourOfAKind extends YatzyHand {
+    static calculate(dice, diceFrequencies) {
+        let fourKindResult = 0;
+        for (const i of this.diceValues) {
+            if (diceFrequencies[i] >= 4) {
+                fourKindResult = i * 4;
+                break;
+            }
+        }
+        return fourKindResult;
+    }
+}
+
+class SmallStraight extends YatzyHand {
+    static calculate(dice, diceFrequencies) {
+        // score if dice contains 1,2,3,4,5
+        let smallStraightResult = 0;
+        let count = 0;
+        for (const frequency of Object.values(diceFrequencies)) {
+            if (frequency === 1) {
+                count++;
+            }
+        }
+        if (count === 5 && diceFrequencies[6] === 0) {
+            for (const die of dice) {
+                smallStraightResult += die;
+            }
+        }
+        return smallStraightResult;
+    }
+}
+
+class LargeStraight extends YatzyHand {
+    static calculate(dice, diceFrequencies) {
+        let largeStraightResult = 0;
+        let straightCount = 0;
+        for (const frequency of Object.values(diceFrequencies)) {
+            if (frequency === 1) {
+                straightCount++;
+            }
+        }
+        if (straightCount === 5 && diceFrequencies[1] === 0) {
+            for (const die of dice) {
+                largeStraightResult += die;
+            }
+        }
+        return largeStraightResult;
+    }
+}
+
+class TwoPairs extends YatzyHand {
+    static calculate(dice, diceFrequencies) {
+        // score if there are two pairs
+        let twoPairResult = 0;
+        let pairCount = 0;
+        for (const frequency of Object.values(diceFrequencies)) {
+            if (frequency >= 2) {
+                pairCount++;
+            }
+        }
+        if (pairCount === 2) {
+            for (const i of this.diceValues) {
+                if (diceFrequencies[i] >= 2) {
+                    twoPairResult += i * 2;
+                }
+            }
+        }
+        return twoPairResult;
+    }
+}
+
+class FullHouse extends YatzyHand {
+    static calculate(dice, diceFrequencies) {
+        // score if there is a pair as well as three of a kind
+        let fullHouseResult = 0;
+        if (Object.values(diceFrequencies).includes(2) &&
+            Object.values(diceFrequencies).includes(3)) {
+            for (const die of dice) {
+                fullHouseResult += die;
+            }
+        }
+        return fullHouseResult;
+    }
+}
+
+module.exports = {
+    Chance, Yatzy,
+    Ones, Twos, Threes, Fours, Fives, Sixes,
+    Pair, TwoPairs, ThreeOfAKind, FourOfAKind, FullHouse,
+    SmallStraight, LargeStraight,
+};
--- javascript/test/yatzy2-test.js
+++ javascript/test/yatzy2-test.js
@@ -1,122 +1,109 @@
 var assert = require("assert");
-var {Yatzy, Chance, Yatzy2, Ones, Twos, Threes, Fours, Fives, Sixes} = require("../lib/yatzy2");
+var {
+    Chance, Yatzy,
+    Ones, Twos, Threes, Fours, Fives, Sixes,
+    Pair, TwoPairs, ThreeOfAKind, FourOfAKind, FullHouse,
+    SmallStraight, LargeStraight,
+} = require("../lib/yatzy2");
 
-
-describe('Yatzy', function() {
-    it('Chance scores sum of all dice', function(){
-        const expected = 15;
-        const actual = Chance.score([2, 3, 4, 5, 1]);
-        assert.equal(expected, actual);
+describe('Yatzy', function () {
+    it('Chance scores sum of all dice', function () {
+        assert.equal(15, Chance.score([2, 3, 4, 5, 1]));
         assert.equal(16, Chance.score([3, 3, 4, 5, 1]));
     });
 
-    
-    it('yatzy_scores_50', function()
-    {
-        assert.equal(50, Yatzy2.score([ 4, 4, 4, 4, 4 ]));
-        assert.equal(50, Yatzy2.score([ 6, 6, 6, 6, 6 ]));
-        assert.equal(0, Yatzy2.score([ 6, 6, 6, 6, 3 ]));
+
+    it('yatzy_scores_50', function () {
+        assert.equal(50, Yatzy.score([4, 4, 4, 4, 4]));
+        assert.equal(50, Yatzy.score([6, 6, 6, 6, 6]));
+        assert.equal(0, Yatzy.score([6, 6, 6, 6, 3]));
     })
 
-    
-    it('test_1s', function()
-    {
-        assert.equal(1, Ones.score([ 1, 2, 3, 4, 5 ]));
-        assert.equal(2, Ones.score([ 1, 2, 1, 4, 5 ]));
-        assert.equal(0, Ones.score([ 6, 2, 2, 4, 5 ]));
-        assert.equal(4, Ones.score([ 1, 2, 1, 1, 1 ]));
+
+    it('test_1s', function () {
+        assert.equal(1, Ones.score([1, 2, 3, 4, 5]));
+        assert.equal(2, Ones.score([1, 2, 1, 4, 5]));
+        assert.equal(0, Ones.score([6, 2, 2, 4, 5]));
+        assert.equal(4, Ones.score([1, 2, 1, 1, 1]));
     })
 
-    
-    it('twos', function()
-    {
-        assert.equal(4, Twos.score([ 1, 2, 3, 2, 6 ]));
-        assert.equal(10, Twos.score([ 2, 2, 2, 2, 2 ]));
+
+    it('twos', function () {
+        assert.equal(4, Twos.score([1, 2, 3, 2, 6]));
+        assert.equal(10, Twos.score([2, 2, 2, 2, 2]));
     })
 
-    
-    it('threes', function()
-    {
-        assert.equal(6, Threes.score([ 1, 2, 3, 2, 3 ]));
-        assert.equal(12, Threes.score([ 2, 3, 3, 3, 3 ]));
+
+    it('threes', function () {
+        assert.equal(6, Threes.score([1, 2, 3, 2, 3]));
+        assert.equal(12, Threes.score([2, 3, 3, 3, 3]));
     })
 
-    
-    it('fours', function()
-    {
-        assert.equal(12, Fours.score([ 4, 4, 4, 5, 5 ]));
-        assert.equal(8, Fours.score([ 4, 4, 5, 5, 5 ]));
-        assert.equal(4, Fours.score([ 4, 5, 5, 5, 5 ]));
+
+    it('fours', function () {
+        assert.equal(12, Fours.score([4, 4, 4, 5, 5]));
+        assert.equal(8, Fours.score([4, 4, 5, 5, 5]));
+        assert.equal(4, Fours.score([4, 5, 5, 5, 5]));
     })
 
-    
-    it('fives', function()
-    {
-        assert.equal(10, Fives.score([ 4, 4, 4, 5, 5 ]));
-        assert.equal(15, Fives.score([ 4, 4, 5, 5, 5 ]));
-        assert.equal(20, Fives.score([ 4, 5, 5, 5, 5 ]));
+
+    it('fives', function () {
+        assert.equal(10, Fives.score([4, 4, 4, 5, 5]));
+        assert.equal(15, Fives.score([4, 4, 5, 5, 5]));
+        assert.equal(20, Fives.score([4, 5, 5, 5, 5]));
     })
 
-    
-    it('sixes', function()
-    {
-        assert.equal(0, Sixes.score([ 4, 4, 4, 5, 5 ]));
-        assert.equal(6, Sixes.score([ 4, 4, 6, 5, 5 ]));
-        assert.equal(18, Sixes.score([ 6, 5, 6, 6, 5 ]));
+
+    it('sixes', function () {
+        assert.equal(0, Sixes.score([4, 4, 4, 5, 5]));
+        assert.equal(6, Sixes.score([4, 4, 6, 5, 5]));
+        assert.equal(18, Sixes.score([6, 5, 6, 6, 5]));
     })
 
-    
-    it('pair', function()
-    {
-        assert.equal(6, new Yatzy().score([ 3, 4, 3, 5, 6 ], "PAIR"));
-        assert.equal(10, new Yatzy().score([ 5, 3, 3, 3, 5 ], "PAIR"));
-        assert.equal(12, new Yatzy().score([ 5, 3, 6, 6, 5 ], "PAIR"));
+
+    it('pair', function () {
+        assert.equal(6, Pair.score([3, 4, 3, 5, 6]));
+        assert.equal(10, Pair.score([5, 3, 3, 3, 5]));
+        assert.equal(12, Pair.score([5, 3, 6, 6, 5]));
     })
 
-    
-    it('two_pair', function()
-    {
-        assert.equal(16, new Yatzy().score([ 3, 3, 5, 4, 5 ], "TWO_PAIRS"));
-        assert.equal(16, new Yatzy().score([ 3, 3, 5, 5, 5 ], "TWO_PAIRS"));
+
+    it('two_pair', function () {
+        assert.equal(16, TwoPairs.score([3, 3, 5, 4, 5]));
+        assert.equal(16, TwoPairs.score([3, 3, 5, 5, 5]));
     })
 
-    
-    it('three_of_a_kind', function()
-    {
-        assert.equal(9, new Yatzy().score([ 3, 3, 3, 4, 5 ], "THREE_OF_A_KIND"));
-        assert.equal(15, new Yatzy().score([ 5, 3, 5, 4, 5 ], "THREE_OF_A_KIND"));
-        assert.equal(9, new Yatzy().score([ 3, 3, 3, 3, 5 ], "THREE_OF_A_KIND"));
+
+    it('three_of_a_kind', function () {
+        assert.equal(9, ThreeOfAKind.score([3, 3, 3, 4, 5]));
+        assert.equal(15, ThreeOfAKind.score([5, 3, 5, 4, 5]));
+        assert.equal(9, ThreeOfAKind.score([3, 3, 3, 3, 5]));
     })
 
-    
-    it('four_of_a_knd', function()
-    {
-        assert.equal(12, new Yatzy().score([ 3, 3, 3, 3, 5 ], "FOUR_OF_A_KIND"));
-        assert.equal(20, new Yatzy().score([ 5, 5, 5, 4, 5 ], "FOUR_OF_A_KIND"));
-        assert.equal(12, new Yatzy().score([ 3, 3, 3, 3, 3 ], "FOUR_OF_A_KIND"));
+
+    it('four_of_a_knd', function () {
+        assert.equal(12, FourOfAKind.score([3, 3, 3, 3, 5]));
+        assert.equal(20, FourOfAKind.score([5, 5, 5, 4, 5]));
+        assert.equal(12, FourOfAKind.score([3, 3, 3, 3, 3]));
     })
 
-    
-    it('smallStraight', function()
-    {
-        assert.equal(15, new Yatzy().score([ 1, 2, 3, 4, 5 ], "SMALL_STRAIGHT"));
-        assert.equal(15, new Yatzy().score([ 2, 3, 4, 5, 1 ], "SMALL_STRAIGHT"));
-        assert.equal(0, new Yatzy().score([ 1, 2, 2, 4, 5 ], "SMALL_STRAIGHT"));
+
+    it('smallStraight', function () {
+        assert.equal(15, SmallStraight.score([1, 2, 3, 4, 5]));
+        assert.equal(15, SmallStraight.score([2, 3, 4, 5, 1]));
+        assert.equal(0, SmallStraight.score([1, 2, 2, 4, 5]));
     })
 
-    
-    it('largeStraight', function()
-    {
-        assert.equal(20, new Yatzy().score([ 6, 2, 3, 4, 5 ], "LARGE_STRAIGHT"));
-        assert.equal(20, new Yatzy().score([ 2, 3, 4, 5, 6 ], "LARGE_STRAIGHT"));
-        assert.equal(0, new Yatzy().score([ 1, 2, 2, 4, 5 ], "LARGE_STRAIGHT"));
+
+    it('largeStraight', function () {
+        assert.equal(20, LargeStraight.score([6, 2, 3, 4, 5]));
+        assert.equal(20, LargeStraight.score([2, 3, 4, 5, 6]));
+        assert.equal(0, LargeStraight.score([1, 2, 2, 4, 5]));
     })
 
-    
-    it('fullHouse', function()
-    {
-        assert.equal(18, new Yatzy().score([ 6, 2, 2, 2, 6 ], "FULL_HOUSE"));
-        assert.equal(0, new Yatzy().score([ 2, 3, 4, 5, 6 ], "FULL_HOUSE"));
+
+    it('fullHouse', function () {
+        assert.equal(18, FullHouse.score([6, 2, 2, 2, 6]));
+        assert.equal(0, FullHouse.score([2, 3, 4, 5, 6]));
     })
 });
-
